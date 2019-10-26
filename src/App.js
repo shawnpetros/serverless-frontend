01b392edb63bff279480f124534c766a2ee26044
Shawn Petros <spetros@gmrmarketing.com>
2019-10-26 10:24:53 -0500

    setting up the client for hooking up to api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Routes from "./Routes";
 import "./App.css";
 
 export default props => (
-  <div className="App container">
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>
-        <Link to="/">Scratch</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar>
-  </div>
+    <div className="App container">
+        <Navbar collapseOnSelect fixed="top">
+            <LinkContainer to="/">
+                <Navbar.Brand>Scratch</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <LinkContainer to="/signup">
+                        <Nav.Link>Sign Up</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/signin">
+                        <Nav.Link>Sign In</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+    </div>
 );
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "shawns-notes-app-upload"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://w9w12khn0l.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_ovnbkH1zR",
+        APP_CLIENT_ID: "2f7h8jtkf6u62uk39ivq6ap2hq",
+        IDENTITY_POOL_ID: "us-east-1:4af69d44-4d8f-4fdc-82ed-24360205ba78"
+    }
+};
